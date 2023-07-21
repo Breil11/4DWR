0eb4b180e1ffd3339a92cd381bb4f22a0105ec32
Breil11 <breilkom@gmail.com>
2023-07-21 22:59:06 +0200

    app files

--- src/components/ProductDetails/ProductDetails.tsx
+++ src/components/ProductDetails/ProductDetails.tsx
@@ -1,6 +1,7 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import "./ProductDetails.css";
 
 interface Product {
   barcode: string;
@@ -21,10 +22,7 @@ function ProductDetails() {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(
-        // `https://world.openfoodfacts.org/api/v2/product/${id}.json`
-        `https://world.openfoodfacts.org/product/${id}`
-      );
+      const response = await axios.get(`https://world.openfoodfacts.org/api/v2/product/${id}.json`);
       const product: Product = response.data.product;
       setProductDetails(product);
     } catch (error) {
@@ -34,14 +32,31 @@ function ProductDetails() {
 
   return (
     <div>
-      <h1> Product Details</h1>
+      <h1>Product Details</h1>
       {productDetails ? (
         <div>
-          <h3>{productDetails.commonName}</h3>
+          {/* <div className="data-container">
+            <span className="data-label">Img:</span>
+            <span className="data-value"></span><img src={productDetails.image_url} alt={productDetails.commonName} />
+          </div> */}
           <img src={productDetails.image_url} alt={productDetails.commonName} />
-          <p>Brand: {productDetails.brands}</p>
-          <p>Ingredients: {productDetails.ingredients_text}</p>
-          <p>Category:{productDetails.categories}</p>
+          <div className="data-container">
+            <span className="data-label">Common Name:</span>
+            <span className="data-value">{productDetails.commonName}</span>
+          </div>
+          <div className="data-container">
+            <span className="data-label">Brands:</span>
+            <span className="data-value">{productDetails.brands}</span>
+          </div>
+          <div className="data-container">
+            <span className="data-label">Ingredients:</span>
+            <span className="data-value">{productDetails.ingredients_text}</span>
+          </div>
+          <div className="data-container">
+            <span className="data-label">Category:</span>
+            <span className="data-value">{productDetails.categories}</span>
+          </div>
+
         </div>
       ) : (
         <p>ca charge...</p>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -6,8 +6,9 @@ function HomePage() {
   return (
     <div className='homepage'>
       <h1>🍅🥦 Welcome to ReactFoodFacts 🥬😜</h1>
-      <h2>Ici, vous pouvez tout simplement rechercher des informations sur des produits alimentaires grace à l'API
-OpenFoodFacts</h2>
+      <h2>
+        Ici, vous pouvez tout simplement rechercher des informations sur des produits alimentaires grace à l'APIOpenFoodFacts
+      </h2>
       <SearchForm />
     </div>
   );
